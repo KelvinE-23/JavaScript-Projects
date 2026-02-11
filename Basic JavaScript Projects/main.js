5bd02fd3629632d03cb69eed6b7331f6ea3cd1ad
KelvinE-23 <essilfiek_22@hotmail.com>
2026-02-11 12:42:26 -0500

    added index.html with module 1 JS assignments and main.js

---
+++ Basic JavaScript Projects/main.js
@@ -0,0 +1,37 @@
+//creating alerts and window alerts//
+alert("Hello, World!");
+window.alert("Hey World, it's me again!");
+
+//utilizing document.writein()//
+document.writeln('Hello World');
+
+var A = "This is a string";
+
+//calling a variable //
+document.writeln(A);
+
+window.alert(A);
+
+document.writeln("<br>Lisa told Sam, \"I think I'm going to get fired!\" <br> Sam said \"I doubt it Lisa, Homer speaks so well of you\"");
+
+// creating a concatenated string //
+var B = "concatenated" + " string";
+document.writeln("<br>", B);
+
+// creating and defining new variables // 
+var Family= "The Richies", Dad= "Dan", Mom= "Lucy", 
+Daughter= "Penny", Son= "Zod";
+
+// creating 2 variables //
+
+var Sent1= "<br>This is the beginning of the string", 
+    Sent2= " and this is the end of the string";
+
+//concatenating the 2 variables// 
+document.writeln(Sent1 + Sent2);
+
+//writing an expression //
+
+var Fams = Family + "Mom and Dad are named: " + Dad + "& " + Mom;
+document.writeln(Fams); 
+
